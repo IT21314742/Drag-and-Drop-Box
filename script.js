df9708302599badc0534cc68177c025097944e09
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-01-17 10:27:36 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ for (list of lists){
         rightBox.addEventListener("dragover", function(e){
             e.preventDefault();
         });
-        rightBox
+        rightBox.addEventListener
     })
 }
