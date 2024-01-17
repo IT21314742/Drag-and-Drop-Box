1f47d1aed21c58c550feac61fd9e925a0bc65200
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-01-17 10:25:27 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ for (list of lists){
     list.addEventListner("dragstart", function(e){
         let selected = e.target;
 
-        rightBox.addEventListener("dragover")
+        rightBox.addEventListener("dragover", )
     })
 }
