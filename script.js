cfb8a275a40828691c08bda0ed84562732619642
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-01-17 10:10:10 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ let lists = document.getElementById("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
 
-for (list of lists)
+for (list of lists){
+    list.
+}
