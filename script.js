c9f0848671755ee68387ea31fc2bab5231685fa6
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-01-18 10:38:21 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ for (list of lists){
 
         leftBox.addEventListener("drop", function(e){
             leftBox.appendChild(selected);
-            selected
+            selected = 
         })
     })
 }
