3d17e7b4e8e4ade094c3150ff56b09b55c5ef4eb
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-01-18 10:40:09 +0530

    FD
    Format the document

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ let lists = document.getElementById("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
 
-for (list of lists){
-    list.addEventListner("dragstart", function(e){
+for (list of lists) {
+    list.addEventListner("dragstart", function (e) {
         let selected = e.target;
 
-        rightBox.addEventListener("dragover", function(e){
+        rightBox.addEventListener("dragover", function (e) {
             e.preventDefault();
         });
-        rightBox.addEventListener("drop", function(e){
+        rightBox.addEventListener("drop", function (e) {
             rightBox.appendChild(selected);
             selected = null;
         });
 
-        leftBox.addEventListener("dragover", function(e){
+        leftBox.addEventListener("dragover", function (e) {
             e.preventDefault();
         });
 
-        leftBox.addEventListener("drop", function(e){
+        leftBox.addEventListener("drop", function (e) {
             leftBox.appendChild(selected);
             selected = null;
         });
